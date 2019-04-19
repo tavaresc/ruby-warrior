a228c4e2842843b82c645c94f1c30a199b5d4311
Claudia Tavares <claudia.tavares@fabernovel.com>
2019-04-19 16:21:39 +0200

    level 2 - done

--- rubywarrior/lulu-beginner/player.rb
+++ rubywarrior/lulu-beginner/player.rb
@@ -1,5 +1,24 @@
 class Player
-  def play_turn(warrior)
+	attr_accessor :warrior
+
+	#def initialize(warrior)
+	#	@warrior = warrior
+	#end
+
+	def solve_level1
   	warrior.walk!
+	end
+
+	def solve_level2
+		if warrior.feel.empty?
+			warrior.walk!
+		else
+			warrior.attack!
+		end
+	end
+
+  def play_turn(warrior)
+  	@warrior = warrior
+  	solve_level2
   end
 end
