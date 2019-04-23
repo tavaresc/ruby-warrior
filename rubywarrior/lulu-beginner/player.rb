0015a4f201c0b1c480fe89bbc42a915c7ec64c72
Claudia Tavares <claudia.tavares@fabernovel.com>
2019-04-23 16:14:33 +0200

    level 6 - done

--- rubywarrior/lulu-beginner/player.rb
+++ rubywarrior/lulu-beginner/player.rb
@@ -1,14 +1,14 @@
 class Player
 	FULL_HEALTH = 20
 
-	attr_accessor :warrior, :health
+	attr_accessor :warrior
 
-	#def initialize(warrior)
-	#	@warrior = warrior
-	#end
+	def need_rest?(current_health, previous_health)
+		return current_health < FULL_HEALTH && current_health >= previous_health
+	end
 
 	def solve_level_1
-  	warrior.walk!
+		warrior.walk!
 	end
 
 	def solve_level_2
@@ -35,7 +35,7 @@ class Player
 			warrior.attack!
 		else
 			# "current_health > previous_health" means that the warrior has been
-			# resting
+			# resting (and can keep it)
 			# "current_health = previous_health" means that the warrior got no
 			# damages in the previous turn (health kept the same during 2 turns) 
 			if @current_health < FULL_HEALTH && @current_health >= @previous_health
@@ -63,8 +63,46 @@ class Player
 		@previous_health = @current_health
 	end
 
-  def play_turn(warrior)
-  	@warrior = warrior
-  	solve_level_5
-  end
+	def solve_level_6
+		@current_health = warrior.health
+		@previous_health = @current_health unless @previous_health
+		@direction = :backward unless @direction
+
+		def change_direction
+			result = 
+			if @direction == :backward
+				:forward
+			else
+				:backward
+			end
+			return result
+		end
+
+		def call_actions(space)
+			if space.enemy?
+				warrior.attack!(@direction)
+			elsif space.captive?
+				warrior.rescue!(@direction)
+			elsif !(space.stairs?) && need_rest?(@current_health, @previous_health)
+				warrior.rest!
+			else
+				warrior.walk!(@direction)
+			end
+		end
+
+		def must_change_direction?
+			return warrior.feel(@direction).wall? || need_rest?(@current_health, @previous_health)
+		end
+
+		call_actions(warrior.feel(@direction))
+
+		@direction = change_direction if must_change_direction?
+		@previous_health = @current_health
+
+	end
+
+	def play_turn(warrior)
+		@warrior = warrior
+		solve_level_6
+	end
 end
